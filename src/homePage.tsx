9b0008d2b48e1ec54d9b08087b92c0bc577f66ff
httpshinigami <miyo.lee@icloud.com>
2024-06-03 00:43:54 +1000

    in the process of integrate search on home page with viewallimages page

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -39,135 +39,185 @@
   };
   */
 
-
-
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './homePage.css';
-
-function parseJwt(token: string) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-
-  return JSON.parse(jsonPayload);
-}
-
-const HomePage: React.FC = () => {
-  const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState<any>(null);
-  const [searchQuery, setSearchQuery] = useState<string>('');
-
-  useEffect(() => {
-    const idToken = sessionStorage.getItem('idToken');
-    if (idToken) {
-      const parsedToken = parseJwt(idToken);
-      setUserInfo(parsedToken);
-      document.title = `Goldfish PixTag`;
-    } else {
+  import React, { useEffect, useState } from 'react';
+  import { useNavigate, useLocation } from 'react-router-dom';
+  import './homePage.css';
+  
+  function parseJwt(token: string) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  
+    return JSON.parse(jsonPayload);
+  }
+  
+  const HomePage: React.FC = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [userInfo, setUserInfo] = useState<any>(null);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [searchResults, setSearchResults] = useState<string[]>([]);
+  
+    useEffect(() => {
+      const idToken = sessionStorage.getItem('idToken');
+      if (idToken) {
+        const parsedToken = parseJwt(idToken);
+        setUserInfo(parsedToken);
+        document.title = `Goldfish PixTag`;
+      } else {
+        navigate('/login');
+      }
+    }, [navigate]);
+  
+    const handleLogout = () => {
+      sessionStorage.clear();
       navigate('/login');
-    }
-  }, [navigate]);
+    };
+  
+    const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+  
+      try {
+        const idToken = sessionStorage.getItem('idToken');
+        const tagsWithCounts = searchQuery.split(',').map(tag => tag.trim());
+  
+        const tags = tagsWithCounts.reduce((acc, tagCount) => {
+          const match = tagCount.match(/^(.*)\s(\d+)$/);
+          if (match) {
+            const [, tag, count] = match;
+            acc[tag.trim()] = parseInt(count, 10);
+          } else {
+            acc[tagCount.trim()] = 1;
+          }
+          console.log(acc);
+          return acc;
+        }, {});
 
-  const handleLogout = () => {
-    sessionStorage.clear();
-    navigate('/login');
-  };
-
-  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    navigate('/viewallimages', { state: { searchQuery } });
-  };
-
-  const handleUpload = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const fileInput = document.getElementById('fileToUpload') as HTMLInputElement;
-    if (!fileInput.files?.length) {
+        const response = await fetch('https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/query_by_tags', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${idToken}`
+          },
+          body: JSON.stringify({ tags })
+        });
+  
+        const result = await response.json();
+        setSearchResults(result.links);
+        // Log the list of URLs to the console
+        console.log('Search Results URLs:', result.links);
+        navigate('/searchresults', { state: { links: result.links } });
+      } catch (error) {
+        console.error('Error during search:', error);
+        alert('An error occurred while searching for images.');
+      }
+    };
+  
+    const handleUpload = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+  
+      const fileInput = document.getElementById('fileToUpload') as HTMLInputElement;
+      if (!fileInput || !fileInput.files || fileInput.files.length === 0) {
         alert('Please select a file to upload.');
         return;
-    }
-
-    const file = fileInput.files[0];
-    const reader = new FileReader();
-
-    reader.onload = async function () {
+      }
+  
+      const file = fileInput.files[0];
+      const reader = new FileReader();
+  
+      reader.onload = async function () {
         if (typeof reader.result !== 'string') {
-            console.error('Error: FileReader result is not a string.');
-            alert('An error occurred while reading the file.');
-            return;
+          console.error('Error: FileReader result is not a string.');
+          alert('An error occurred while reading the file.');
+          return;
         }
-
+  
         console.log('File read successfully.');
         console.log('Sending request to API Gateway...');
-
+  
         try {
-            const base64Content = reader.result.split(',')[1]; // Remove the base64 prefix
-            const idToken = sessionStorage.getItem('idToken'); // Retrieve idToken from sessionStorage
-
-            const response = await fetch('https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/upload', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/octet-stream',
-                    'x-user-id': userInfo['cognito:username'], // Add user name as a header
-                    'Authorization': `Bearer ${idToken}` // Add authorization token
-                },
-                body: base64Content
-            });
-
-            console.log('Received response from API Gateway.');
-            const result = await response.json();
-            alert(result);
+          const base64Content = reader.result.split(',')[1];
+          const idToken = sessionStorage.getItem('idToken');
+  
+          const response = await fetch('https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/upload', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/octet-stream',
+              'x-user-id': userInfo['cognito:username'],
+              'Authorization': `Bearer ${idToken}`
+            },
+            body: base64Content
+          });
+  
+          console.log('Received response from API Gateway.');
+          const result = await response.json();
+          alert(result);
         } catch (error) {
-            console.error('Error:', error);
-            alert('An error occurred while uploading the image.');
+          console.error('Error:', error);
+          alert('An error occurred while uploading the image.');
         }
-    };
-
-    reader.onerror = function () {
+      };
+  
+      reader.onerror = function () {
         console.error('Error reading file:', reader.error);
         alert('Failed to read file.');
+      };
+  
+      reader.readAsDataURL(file);
     };
-
-    reader.readAsDataURL(file);
-  };
-
-  const handleDelete = () => {
-    navigate('/delete');
-  };
-
-  const handleViewAllImages = () => {
-    navigate('/viewallimages');
-  };
-
-  return (
-    <div id="homepage">
-      {userInfo && <h1>Welcome, {userInfo.given_name}</h1>}
-      <form onSubmit={handleSearch} className="mb-3">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search for images..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button className="btn btn-outline-secondary" type="submit">Search</button>
-        </div>
-      </form>
-      <form onSubmit={handleUpload} className="mb-3">
-        <input type="file" name="fileToUpload" id="fileToUpload" className="form-control" />
-        <button type="submit" className="btn btn-primary mt-2">Upload Image</button>
-      </form>
-      <div className="btn-group" role="group" aria-label="Basic example">
-        <button onClick={handleViewAllImages} className="btn btn-secondary">Image Gallery</button>
-        <button onClick={handleDelete} className="btn btn-danger">Delete Images</button>
+  
+    const handleDelete = () => {
+      navigate('/delete');
+    };
+  
+    const handleViewAllImages = () => {
+      navigate('/viewallimages');
+    };
+  
+    return (
+      <div id="homepage">
+        {userInfo && <h1>Welcome, {userInfo.given_name}</h1>}
+        <form onSubmit={handleSearch} className="mb-3">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search for images..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className="btn btn-outline-secondary" type="submit">Search</button>
+          </div>
+        </form>
+        {searchResults.length > 0 ? (
+          <div className="search-results">
+            <h2>Search Results:</h2>
+            <ul className="list-unstyled">
+              {searchResults.map((url, index) => (
+                <li key={index}>
+                  <img src={url} alt={`Thumbnail ${index}`} className="img-thumbnail" />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No search results found.</p>
+        )}
+        <form onSubmit={handleUpload}>
+          <div className="mb-3">
+            <label htmlFor="fileToUpload" className="form-label">Upload an image</label>
+            <input className="form-control" type="file" id="fileToUpload" />
+          </div>
+          <button type="submit" className="btn btn-primary">Upload</button>
+        </form>
+        <button className="btn btn-secondary" onClick={handleDelete}>Delete</button>
+        <button className="btn btn-secondary" onClick={handleViewAllImages}>View All Images</button>
+        <button className="btn btn-secondary" onClick={handleLogout}>Logout</button>
       </div>
-    </div>
-  );
-};
-
-export default HomePage;
+    );
+  };
+  
+  export default HomePage;
+  
--- src/viewAllImages.tsx
+++ src/viewAllImages.tsx
@@ -164,85 +164,62 @@ const ViewAllImagesPage = () => {
 export default ViewAllImagesPage;
 */
 
+
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const ViewAllImagesPage = () => {
   const location = useLocation();
-  const searchQuery = location.state?.searchQuery || '';
   const [thumbnails, setThumbnails] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [copied, setCopied] = useState(false);
-  const [copiedIndex, setCopiedIndex] = useState(null); // To track which URL was copied
+  const [copiedIndex, setCopiedIndex] = useState(null);  // To track which URL was copied
+
+  useEffect(() => {
+    if (location.state && location.state.searchResults) {
+      setThumbnails(location.state.searchResults.map(url => ({ thumbnailUrl: url, tags: [] }))); // Assuming search results are just URLs
+    } else {
+      fetchThumbnails();
+    }
+  }, [location.state]);
+
+  const fetchThumbnails = async () => {
+    const userName = getUsernameFromToken();
+    if (!userName) return;
+    
+    setIsLoading(true);
+    setError('');
+    try {
+      const response = await fetch(`https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/viewallimages?userName=${userName}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch thumbnails');
+      }
+      const data = await response.json();
+      const parsedBody = JSON.parse(data.body);
+      console.log(parsedBody)
+      setThumbnails(parsedBody.images);
+    } catch (err) {
+      console.error('Error fetching thumbnails:', err);
+      setError('Failed to load thumbnails.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const getUsernameFromToken = () => {
     const token = sessionStorage.getItem('idToken');
     if (token) {
-      const base64Url = token.split('.')[1]; // Get the payload part
+      const base64Url = token.split('.')[1];
       const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
       const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       }).join(''));
-
-      return JSON.parse(jsonPayload)['cognito:username']; // Adjust based on the actual key where username is stored
+      return JSON.parse(jsonPayload)['cognito:username'];
     }
     return null;
   };
-
-  const userName = getUsernameFromToken();
-
-  useEffect(() => {
-    if (userName && searchQuery) {
-      const fetchThumbnails = async () => {
-        setIsLoading(true);
-        setError('');
-        try {
-          const idToken = sessionStorage.getItem('idToken');
-          const tagsWithCounts = searchQuery.split(',').map(tag => tag.trim());
-
-          const tags = tagsWithCounts.reduce((acc, tagCount) => {
-            const match = tagCount.match(/^(.*)\s(\d+)$/);
-            if (match) {
-              const [, tag, count] = match;
-              acc[tag.trim()] = parseInt(count, 10);
-            } else {
-              acc[tagCount.trim()] = 1;
-            }
-            return acc;
-          }, {});
-
-          const response = await fetch('https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/query_by_tags', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${idToken}`
-            },
-            body: JSON.stringify({ tags })
-          });
-
-          if (!response.ok) {
-            throw new Error('Failed to fetch thumbnails');
-          }
-
-          const data = await response.json();
-          setThumbnails(data.links);
-        } catch (err) {
-          console.error('Error fetching thumbnails:', err);
-          setError('Failed to load thumbnails.');
-        } finally {
-          setIsLoading(false);
-        }
-      };
-
-      fetchThumbnails();
-    }
-  }, [userName, searchQuery]);
-
-  if (!userName) return <p>Please log in to view images.</p>;
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
 
   const handleCopy = (index) => {
     setCopied(true);
@@ -253,65 +230,92 @@ const ViewAllImagesPage = () => {
     }, 2000);
   };
 
-  const handleViewImage = async (thumbnailUrl: string) => {
-    const cleanedUrl = thumbnailUrl.replace("_thumbnail", "").replace("goldfishthumbnails", "goldfishimages");
+  const handleViewImage = async (thumbnailUrl) => {
     try {
-      const response = await fetch(cleanedUrl, {
-        method: 'GET'
+      const idToken = sessionStorage.getItem('idToken');
+      const response = await fetch('https://tw6nv3lpxl.execute-api.us-east-1.amazonaws.com/prod/viewimage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${idToken}`
+        },
+        body: JSON.stringify({ thumbnailUrl })
       });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}, Status Text: ${response.statusText}`);
+  
+      if (response.ok) {
+        const data = await response.json();
+        const imageUrl = data.url;
+        window.open(imageUrl, '_blank');
+      } else {
+        console.error('Error viewing image:', response.statusText);
+        alert('Failed to view image.');
       }
-
-      const imageData = await response.blob();
-      console.log("Image Data:", imageData);
-      // Handle image data as needed, e.g., displaying it in a modal or new page
-    } catch (err) {
-      console.error('Error fetching image:', err);
+    } catch (error) {
+      console.error('Error during viewing image:', error);
+      alert('An error occurred while viewing the image.');
     }
   };
+  
 
-  return (
+  
+return (
     <div>
-      <main>
-        <section className="py-1 text-center container">
-          <div className="row py-lg-5">
-            <div className="col-lg-4 col-md-4 mx-auto">
-              <h1 className="fw-light">Photo Gallery</h1>
-            </div>
-          </div>
-        </section>
-        <div className="album py-5 bg-light">
-          <div className="container">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              {Array.isArray(thumbnails) && thumbnails.map((url, index) => (
-                <div key={index} className="col">
-                  <div className="card shadow-sm">
-                    <img src={url} className="bd-placeholder-img card-img-top" alt={`Thumbnail ${index}`} style={{ objectFit: 'cover', height: '225px' }} />
-                    <div className="card-body">
-                      <div className="d-flex justify-content-between align-items-center">
-                        <div className="btn-group">
-                          <CopyToClipboard text={url} onCopy={() => handleCopy(index)}>
-                            <button type="button" className="btn btn-secondary">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
-                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
-                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                              </svg>
-                            </button>
-                          </CopyToClipboard>
-                          <button className="btn btn-sm btn-outline-secondary" onClick={() => handleViewImage(url)}>View</button>
-                        </div>
-                        {copied && copiedIndex === index && <span style={{ color: 'green' }}>Copied!</span>}
-                      </div>
+        <main>
+            <section className="py-1 text-center container">
+                <div className="row py-lg-5">
+                    <div className="col-lg-4 col-md-4 mx-auto">
+                        <h1 className="fw-light">Photo Gallery</h1>
                     </div>
-                  </div>
+                    <form onSubmit={() => { }} className="mb-3">
+                        <div className="input-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Search for images..."
+                            // value={searchQuery}
+                            // onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                            <button className="btn btn-outline-secondary" type="submit">Search</button>
+                        </div>
+                    </form>
                 </div>
-              ))}
+
+            </section>
+
+            <div className="album py-5 bg-light">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        {Array.isArray(thumbnails) && thumbnails.map((image, index) => (
+                            <div key={index} className="col">
+                                <div className="card shadow-sm">
+                                    <img src={image.thumbnailUrl} className="bd-placeholder-img card-img-top" alt={`Thumbnail ${index}`} style={{ objectFit: 'cover', height: '225px' }} />
+                                    <div className="card-body">
+                                        <p className="card-text">Tags: {image.tags.length > 0 ? image.tags.join(', ') : 'No tag identified'}</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="btn-group">
+                                                <CopyToClipboard text={image.thumbnailUrl} onCopy={() => handleCopy(index)}>
+                                                    <button type="button" className="btn btn-secondary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
+                                                            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+                                                            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                                                        </svg>
+                                                    </button>
+                                                </CopyToClipboard>
+                                                /* need to be implemented  */
+                                               /* UNCOMMENT
+                                               <a href={image.thumbnailUrl} className="btn btn-sm btn-outline-secondary" target="_blank" rel="noopener noreferrer">View</a>
+                                            </div>
+
+                                            {copied && copiedIndex === index && <span style={{ color: 'green' }}>Copied!</span>}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </main>
+        </main>
     </div>
   );
 };
